ce12011765e0a5b2cc07927d416a63d58de8310a
David Ellis <david.ellis@unmc.edu>
2017-04-07 08:27:06 -0500

    FIX: converts range to list

--- generator.py
+++ generator.py
@@ -23,7 +23,7 @@ def get_validation_split(data_file, data_split=0.8, overwrite=False):
     if overwrite or not os.path.exists(config["training_file"]):
         print("Creating validation split...")
         nb_samples = data_file.root.data.shape[0]
-        sample_list = range(nb_samples)
+        sample_list = list(range(nb_samples))
         training_list, testing_list = split_list(sample_list, split=data_split)
         pickle_dump(training_list, config["training_file"])
         pickle_dump(testing_list, config["testing_file"])
