5cf8fba550a25b9ab86331c2b01b89b01794ed01
David Ellis <david.ellis@unmc.edu>
2017-08-08 17:09:35 -0500

    TEST: Adds more testing for reconstructing patches

--- test/test_predict.py
+++ test/test_predict.py
@@ -10,9 +10,7 @@ class TestPrediction(TestCase):
     def setUp(self):
         image_shape = (120, 144, 90)
         data = np.arange(0, image_shape[0]*image_shape[1]*image_shape[2]).reshape(image_shape)
-        spacing = (1, 0.5, 1.5)
         affine = np.diag(np.ones(4))
-        affine[:3, -1] = spacing
         self.image = nib.Nifti1Image(data, affine)
 
     def test_reconstruct_from_patches(self):
@@ -37,3 +35,18 @@ class TestPrediction(TestCase):
         # The resulting reconstruction should be the average.
         # noinspection PyTypeChecker
         self.assertTrue(np.all((self.image.get_data() - 1) == reconstruced_data))
+
+    def test_reconstruct_with_overlapping_patches2(self):
+        image_shape = (144, 144, 144)
+        data = np.arange(0, image_shape[0]*image_shape[1]*image_shape[2]).reshape(image_shape)
+        patch_overlap = 16
+        patch_shape = (64, 64, 64)
+        patch_indices = compute_patch_indices(data.shape, patch_shape, patch_overlap)
+        patches = [get_patch_from_3d_data(data, patch_shape, index) for index in patch_indices]
+
+        no_overlap_indices = compute_patch_indices(data.shape, patch_shape, 32)
+        patch_indices = np.concatenate([patch_indices, no_overlap_indices])
+        patches.extend([get_patch_from_3d_data(data, patch_shape, index) for index in no_overlap_indices])
+        reconstruced_data = reconstruct_from_patches(patches, patch_indices, data.shape)
+        # noinspection PyTypeChecker
+        self.assertTrue(np.all(data == reconstruced_data))
