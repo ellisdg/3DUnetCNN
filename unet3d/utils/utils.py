4a401772ba33c64375bee2035b1fbc8b995d2a11
David Ellis <david.ellis@unmc.edu>
2017-11-15 13:57:01 -0600

    FIX: Undos the last commit

--- unet3d/utils/utils.py
+++ unet3d/utils/utils.py
@@ -9,12 +9,12 @@ from .nilearn_custom_utils.nilearn_utils import crop_img_to
 
 
 def pickle_dump(item, out_file):
-    with open(out_file, "w") as opened_file:
+    with open(out_file, "wb") as opened_file:
         pickle.dump(item, opened_file)
 
 
 def pickle_load(in_file):
-    with open(in_file, "r") as opened_file:
+    with open(in_file, "rb") as opened_file:
         return pickle.load(opened_file)
 
 
