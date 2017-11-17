ce52a34a9b39872ca3270df7236c3d1ab7ceafb4
David Ellis <david.ellis@unmc.edu>
2017-11-17 10:48:56 -0600

    ENH: Now runs all of the validation cases

--- brats/predict.py
+++ brats/predict.py
@@ -2,14 +2,17 @@ import os
 
 from train import config
 from unet3d.prediction import run_validation_case
+from unet3d.utils import pickle_load
 
 
 def main():
     prediction_dir = os.path.abspath("prediction")
-    run_validation_case(test_index=0, out_dir=prediction_dir, model_file=config["model_file"],
-                        validation_keys_file=config["validation_file"],
-                        training_modalities=config["training_modalities"], output_label_map=True,
-                        labels=config["labels"], hdf5_file=config["data_file"])
+    validation_indices = pickle_load(config["validation_file"])
+    for i in range(len(validation_indices)):
+        run_validation_case(test_index=i, out_dir=os.path.join(prediction_dir, "validation_case_{}".format(i)),
+                            model_file=config["model_file"], validation_keys_file=config["validation_file"],
+                            training_modalities=config["training_modalities"], output_label_map=True,
+                            labels=config["labels"], hdf5_file=config["data_file"])
 
 
 if __name__ == "__main__":
