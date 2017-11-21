3ed677fdeec6e77f01cbc60bdcaf0b173de61890
David Ellis <david.ellis@unmc.edu>
2017-11-21 14:45:21 -0600

    ENH: changes batch size to 1

--- brats/train_isensee2017.py
+++ brats/train_isensee2017.py
@@ -23,8 +23,8 @@ else:
 config["truth_channel"] = config["nb_channels"]
 config["deconvolution"] = True  # if False, will use upsampling instead of deconvolution
 
-config["batch_size"] = 2
-config["validation_batch_size"] = 4
+config["batch_size"] = 1
+config["validation_batch_size"] = 2
 config["n_epochs"] = 500  # cutoff the training after this many epochs
 config["patience"] = 10  # learning rate will be reduced after this many epochs if the validation loss is not improving
 config["early_stop"] = 50  # training will be stopped after this many epochs without the validation loss improving
