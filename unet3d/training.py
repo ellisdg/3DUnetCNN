d2e1830b13008b2937e679eaafcc7711bb1a5bbc
David Ellis <david.ellis@unmc.edu>
2017-11-22 13:59:37 -0600

    ENH: Updates model loading to load different function names

--- unet3d/training.py
+++ unet3d/training.py
@@ -5,7 +5,8 @@ from keras import backend as K
 from keras.callbacks import ModelCheckpoint, CSVLogger, LearningRateScheduler, ReduceLROnPlateau, EarlyStopping
 from keras.models import load_model
 
-from unet3d.metrics import dice_coefficient, dice_coefficient_loss
+from unet3d.metrics import (dice_coefficient, dice_coefficient_loss, dice_coef, dice_coef_loss,
+                            weighted_dice_coefficient_loss, weighted_dice_coefficient)
 
 K.set_image_dim_ordering('th')
 
@@ -34,7 +35,10 @@ def get_callbacks(model_file, initial_learning_rate=0.0001, learning_rate_drop=0
 
 def load_old_model(model_file):
     print("Loading pre-trained model")
-    custom_objects = {'dice_coefficient_loss': dice_coefficient_loss, 'dice_coefficient': dice_coefficient}
+    custom_objects = {'dice_coefficient_loss': dice_coefficient_loss, 'dice_coefficient': dice_coefficient,
+                      'dice_coef': dice_coef, 'dice_ceof_loss': dice_coef_loss,
+                      'weighted_dice_coefficient': weighted_dice_coefficient,
+                      'weighted_dice_coefficient_loss': weighted_dice_coefficient_loss}
     try:
         from keras_contrib.layers import Deconvolution3D
         custom_objects["Deconvolution3D"] = Deconvolution3D
