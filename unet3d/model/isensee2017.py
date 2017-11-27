3ececcb4842c691f8baa6e6440c4a0b9a2ccd3eb
David Ellis <david.ellis@unmc.edu>
2017-11-27 09:19:01 -0600

    Switches to sigmoid because softmax doesn't seem to be working

--- unet3d/model/isensee2017.py
+++ unet3d/model/isensee2017.py
@@ -13,7 +13,7 @@ create_convolution_block = partial(create_convolution_block, activation=LeakyReL
 
 def isensee2017_model(input_shape=(4, 128, 128, 128), n_base_filters=16, depth=5, dropout_rate=0.3,
                       n_segmentation_levels=3, n_labels=4, optimizer=Adam, initial_learning_rate=5e-4,
-                      loss_function=weighted_dice_coefficient_loss, activation_name="softmax"):
+                      loss_function=weighted_dice_coefficient_loss, activation_name="sigmoid"):
     """
     This function builds a model proposed by Isensee et al. for the BRATS 2017 competition:
     https://www.cbica.upenn.edu/sbia/Spyridon.Bakas/MICCAI_BraTS/MICCAI_BraTS_2017_proceedings_shortPapers.pdf
--- unet3d/model/unet.py
+++ unet3d/model/unet.py
@@ -16,7 +16,7 @@ except ImportError:
 
 def unet_model_3d(input_shape, pool_size=(2, 2, 2), n_labels=1, initial_learning_rate=0.00001, deconvolution=False,
                   depth=4, n_base_filters=32, include_label_wise_dice_coefficients=False, metrics=dice_coefficient,
-                  batch_normalization=False, activation_name="softmax"):
+                  batch_normalization=False, activation_name="sigmoid"):
     """
     Builds the 3D UNet Keras model.f
     :param metrics: List metrics to be calculated during model training (default is dice coefficient).
