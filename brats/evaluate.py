d194d8abd924932caab53d6e858918a84f3e5b64
David Ellis <david.ellis@unmc.edu>
2017-12-18 12:09:42 -0600

    ENH: Modifies the pyplot graphs

--- brats/evaluate.py
+++ brats/evaluate.py
@@ -39,17 +39,24 @@ def main():
     df = pd.DataFrame.from_records(rows, columns=header)
     df.to_csv("./prediction/brats_scores.csv")
 
-    plt.boxplot(df.values, labels=df.columns)
+    scores = dict()
+    for index, score in enumerate(df.columns):
+        values = df.values.T[index]
+        scores[score] = values[np.isnan(values) == False]
+
+    plt.boxplot(list(scores.values()), labels=list(scores.keys()))
     plt.ylabel("Dice Coefficient")
     plt.savefig("validation_scores_boxplot.png")
     plt.close()
 
     training_df = pd.read_csv("./training.log").set_index('epoch')
 
-    plt.plot(training_df['loss'].values)
-    plt.plot(training_df['val_loss'].values)
+    plt.plot(training_df['loss'].values, label='training loss')
+    plt.plot(training_df['val_loss'].values, label='validation loss')
     plt.ylabel('Loss')
     plt.xlabel('Epoch')
+    plt.xlim((0, len(training_df.index)))
+    plt.legend(loc='upper right')
     plt.savefig('loss_graph.png')
 
 
