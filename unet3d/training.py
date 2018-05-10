e4717ffd1696699a385bae3bd21ea4c286b10345
David Ellis <david.ellis@unmc.edu>
2018-05-10 11:40:05 -0500

    TEST: switch multiprocessing to True

--- unet3d/training.py
+++ unet3d/training.py
@@ -80,6 +80,7 @@ def train_model(model, model_file, training_generator, validation_generator, ste
                         epochs=n_epochs,
                         validation_data=validation_generator,
                         validation_steps=validation_steps,
+                        use_multiprocessing=True,
                         callbacks=get_callbacks(model_file,
                                                 initial_learning_rate=initial_learning_rate,
                                                 learning_rate_drop=learning_rate_drop,
