adbacd2309408744b72f705a24360b818008ed5e
David Ellis <david.ellis@unmc.edu>
2018-05-10 10:18:14 -0500

    Changes normalization method to be image-wise

--- unet3d/normalize.py
+++ unet3d/normalize.py
@@ -70,16 +70,11 @@ def normalize_data(data, mean, std):
 
 
 def normalize_data_storage(data_storage):
-    means = list()
-    stds = list()
     for index in range(data_storage.shape[0]):
         data = data_storage[index]
-        means.append(data.mean(axis=(1, 2, 3)))
-        stds.append(data.std(axis=(1, 2, 3)))
-    mean = np.asarray(means).mean(axis=0)
-    std = np.asarray(stds).mean(axis=0)
-    for index in range(data_storage.shape[0]):
-        data_storage[index] = normalize_data(data_storage[index], mean, std)
+        mean = data.mean(axis=(1, 2, 3))
+        std = data.std(axis=(1, 2, 3))
+        data_storage[index] = normalize_data(data, mean, std)
     return data_storage
 
 
