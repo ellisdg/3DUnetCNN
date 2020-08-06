3a60167b0f37423778d2fa0b164a81ff121cdc4b
David G Ellis <ellisdg@users.noreply.github.com>
2020-08-06 06:47:21 -0500

    STYLE: changes filenames
    Changes filenames of the models and the pickle files that store the training and validation ids.

--- brats/train_isensee2017.py
+++ brats/train_isensee2017.py
@@ -40,9 +40,9 @@ config["training_patch_start_offset"] = (16, 16, 16)  # randomly offset the firs
 config["skip_blank"] = True  # if True, then patches without any target will be skipped
 
 config["data_file"] = os.path.abspath("brats_data.h5")
-config["model_file"] = os.path.abspath("isensee_2017_model.h5")
-config["training_file"] = os.path.abspath("isensee_training_ids.pkl")
-config["validation_file"] = os.path.abspath("isensee_validation_ids.pkl")
+config["model_file"] = os.path.abspath("unet_model.h5")
+config["training_file"] = os.path.abspath("training_ids.pkl")
+config["validation_file"] = os.path.abspath("validation_ids.pkl")
 config["overwrite"] = False  # If True, will previous files. If False, will use previously written files.
 
 
