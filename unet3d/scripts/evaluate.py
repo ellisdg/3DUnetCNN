6c655d2bcd71e2df8004d71ff877909184d359ce
David Ellis <david.ellis@unmc.edu>
2022-05-31 17:24:50 -0500

    FIX

--- unet3d/scripts/evaluate.py
+++ unet3d/scripts/evaluate.py
@@ -77,7 +77,7 @@ def main():
 
     from multiprocessing import Pool
     with Pool(namespace.n_threads) as pool:
-        pool.map(_evaluate_filenames(zip(arange(len(filenames)), filenames)))
+        pool.map(_evaluate_filenames(zip(range(len(filenames)), filenames)))
 
     df = pd.DataFrame(scores, columns=labels, index=subject_ids)
     df.to_csv(namespace.output_filename)
