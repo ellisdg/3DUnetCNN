cce2357983efcbba85a1f3bde75b5586e7ebf8ad
david.ellis <david.ellis@unmc.edu>
2022-07-06 16:03:11 -0500

    DEBUG

--- unet3d/utils/sequences.py
+++ unet3d/utils/sequences.py
@@ -113,13 +113,14 @@ def augment_affine(affine, shape, augment_scale_std=None, augment_scale_probabil
 
 def augment_image(image, augment_blur_mean=None, augment_blur_std=None, augment_blur_probability=1,
                   additive_noise_std=None, additive_noise_probability=1):
-    print("augment 1", image.dataobj.mean())
+    augment1 = image.dataobj.mean()
     if not (augment_blur_mean is None or augment_blur_std is None) and decision(augment_blur_probability):
         image = random_blur(image, mean=augment_blur_mean, std=augment_blur_std)
-    print("augment 2", image.dataobj.mean())
+    augment2 = image.dataobj.mean()
     if additive_noise_std and decision(additive_noise_probability):
         image.dataobj = add_noise(image.dataobj, sigma_factor=additive_noise_std)
-    print("augment 3", image.dataobj.mean())
+    augment3 = image.dataobj.mean()
+    print("Augment:", augment1, augment2, augment3)
     return image
 
 
@@ -129,8 +130,10 @@ def format_feature_image(feature_image, window, crop=False, cropping_kwargs=None
                          augment_translation_probability=0, augment_blur_mean=None, augment_blur_std=None,
                          augment_blur_probability=0, flip_front_back_probability=0, reorder=False,
                          interpolation="linear"):
+    print("Format:", feature_image.dataobj.mean())
     if reorder:
         feature_image = reorder_image(feature_image)
+        print("Reorder:", feature_image.dataobj.mean())
     if crop:
         if cropping_kwargs is None:
             cropping_kwargs = dict()
@@ -466,6 +469,7 @@ class WholeVolumeToSurfaceSequence(HCPRegressionSequence):
 
     def resample_input(self, feature_filename):
         feature_image = load_image(feature_filename, reorder=False, verbose=self.verbose)
+        print("Load:", feature_image.dataobj.mean())
         feature_image, affine = format_feature_image(feature_image=feature_image, window=self.window, crop=self.crop,
                                                      cropping_kwargs=self.cropping_kwargs,
                                                      augment_scale_std=self.augment_scale_std,
