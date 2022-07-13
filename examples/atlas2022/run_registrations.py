ddee2f1c44c33cd03f37a10fe1ffb3cd42a8658a
david.ellis <david.ellis@unmc.edu>
2022-07-13 18:28:22 -0500

    ENH: sycn registrations WIP1

--- examples/atlas2022/run_registrations.py
+++ examples/atlas2022/run_registrations.py
@@ -3,7 +3,8 @@ This script registers each of the atlas cases to the template, and then transfor
 """
 
 import glob
-from nipype import Node, Workflow, IdentityInterface, MapNode
+from nipype import Node, Workflow, IdentityInterface, MapNode, Function
+from nipype.interfaces.ants import ApplyTransform
 from nipype.interfaces.ants.registration import RegistrationSynQuick, RegistrationSynQuickInputSpec, traits
 
 
@@ -38,6 +39,46 @@ class _RegistrationSynQuick(RegistrationSynQuick):
     input_spec = _RegistrationSynQuickInputSpec
 
 
+def mix_n_match(t1_files, mask_files, forward_transforms, inverse_transforms):
+    """
+    Mix and match the transforms to form every combination of transformation possible.
+    """
+    t1_out = list()
+    mask_out = list()
+    transforms_out = list()
+    reference_out = list()
+    for t1, mask, forward in zip(t1_files, mask_files, forward_transforms):
+        for reference, inverse in zip(t1_files, inverse_transforms):
+            if reference != t1:
+                transforms_out.append([inverse, forward])
+                t1_out.append(t1)
+                mask_out.append(mask)
+                reference_out.append(reference)
+    return t1_out, mask_out, transforms_out, reference_out
+
+
+def sync_outputs(t1_files, mask_files, reference_files, warped_t1s, warped_masks, output_dir="./augmented_data"):
+    import shutil
+    import os
+    os.makedirs(output_dir, exist_ok=True)
+    for t1, mask, ref, warped_t1, warped_mask in zip(t1_files, mask_files, reference_files, warped_t1s, warped_masks):
+        t1_sub = t1.split("/")[-4]
+        ref_sub = ref.split("/")[-4]
+        _output_dir = os.path.join(output_dir, "_".join((t1_sub, ref_sub)))
+
+        # copy t1
+        out_t1 = os.path.join(_output_dir, os.path.basename(t1))
+        if os.path.exists(out_t1):
+            os.remove(out_t1)
+        shutil.copy(warped_t1, out_t1)
+
+        # copy mask
+        out_mask = os.path.join(_output_dir, os.path.basename(mask))
+        if os.path.exists(out_mask):
+            os.remove(out_mask)
+        shutil.copy(warped_mask, out_mask)
+
+
 def main():
     wf = Workflow("RegistrationWF")
 
@@ -66,6 +107,25 @@ def main():
     wf.connect(input_node, "target", reg_node, "fixed_image")
     wf.connect(input_node, "t1s", reg_node, "moving_image")
     wf.connect(input_node, "reg_mask_args", reg_node, "args")
+
+    mixer = Node(Function(func=mix_n_match, outputs=["t1_out", "mask_out", "transforms_out", "reference_out"]),
+                 name="MixNMatch")
+    wf.connect(reg_node, "forward_warp_field", mixer, "forward_transforms")
+    wf.connect(reg_node, "inverse_warp_field", mixer, "inverse_transforms")
+    wf.connect(input_node, "t1s", mixer, "t1_files")
+    wf.connect(input_node, "masks", mixer, "mask_files")
+
+    transform_t1s = MapNode(ApplyTransform(), name="TransformT1s",
+                            iterfield=["reference_image", "input_image", "transforms"])
+    wf.connect(mixer, "t1_out", transform_t1s, "input_image")
+    wf.connect(mixer, "reference_out", transform_t1s, "reference_image")
+    wf.connect(mixer, "transforms_out", transform_t1s, "transforms")
+
+    transform_masks = MapNode(ApplyTransform(interpolation="NearestNeighbor"), name="TransformMasks",
+                              iterfield=["reference_image", "input_image", "transforms"])
+    wf.connect(mixer, "mask_out", transform_masks, "input_image")
+    wf.connect(mixer, "reference_out", transform_masks, "reference_image")
+    wf.connect(mixer, "transforms_out", transform_masks, "transforms")
 
     wf.run(plugin="MultiProc", plugin_args={"n_procs": 40})
 
