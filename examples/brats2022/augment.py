378361a9885a014584b448cfa515d919f7b73fba
David Ellis <david.ellis@unmc.edu>
2022-07-15 12:52:43 -0500

    ENH: 10 transforms

--- examples/brats2022/augment.py
+++ examples/brats2022/augment.py
@@ -9,34 +9,7 @@ from nipype.interfaces.ants.registration import RegistrationSynQuick, Registrati
 import os
 
 
-#TODO: add flair, t2, and t1c images
-
-
 class _RegistrationSynQuickInputSpec(RegistrationSynQuickInputSpec):
-    transform_type = traits.Enum(
-        "s",
-        "t",
-        "r",
-        "a",
-        "sr",
-        "so",
-        "b",
-        "br",
-        argstr="-t %s",
-        desc="""\
-    Transform type
-
-      * t:  translation
-      * r:  rigid
-      * a:  rigid + affine
-      * s:  rigid + affine + deformable syn (default)
-      * sr: rigid + deformable syn
-      * b:  rigid + affine + deformable b-spline syn
-      * br: rigid + deformable b-spline syn
-
-    """,
-        usedefault=True,
-    )
     moving_mask = File(argstr="-x Null,%s")
 
 
@@ -44,21 +17,45 @@ class _RegistrationSynQuick(RegistrationSynQuick):
     input_spec = _RegistrationSynQuickInputSpec
 
 
-def mix_n_match(t1_files, mask_files, forward_transforms, inverse_transforms):
+def mix_n_match(t1_file, mask_file, t1c_file, flair_file, t2_file, forward_transform, forward_matrix,
+                inverse_transforms, forward_matrices, reference_files, n=10,
+                output_dir="/work/aizenberg/dgellis/MICCAI/2022/isles/isles_2022/augmented_data"):
     """
     Mix and match the transforms to form every combination of transformation possible.
     """
+    import os
+    from nipype.interfaces.ants import ApplyTransforms
+    import random
+    os.makedirs(output_dir, exist_ok=True)
     t1_out = list()
     mask_out = list()
     transforms_out = list()
     reference_out = list()
-    for t1, mask, forward in zip(t1_files, mask_files, forward_transforms):
-        for reference, inverse in zip(t1_files, inverse_transforms):
-            if reference != t1:
-                transforms_out.append([inverse, forward])
-                t1_out.append(t1)
-                mask_out.append(mask)
-                reference_out.append(reference)
+    output_images = list()
+    _sub = t1_file.split("/")[-2]
+    idx = list(range(len(reference_files)))
+    random.shuffle(idx)
+    idx = idx[:n]  # just do n transformations
+    for i in idx:
+        reference = reference_files[i]
+        inverse = inverse_transforms[i]
+        ref_forward_matrix = forward_matrices[i]
+        ref_sub = reference.split("/")[-2]
+        _output_dir = os.path.join(output_dir, "_".join((_sub, ref_sub)))
+        if reference != t1_file:
+            os.makedirs(_output_dir, exist_ok=True)
+            for input_image in (t1_file, t1c_file, flair_file, t2_file, mask_file):
+                output_image = os.path.join(_output_dir, os.path.basename(input_image))
+                cmd = ApplyTransforms(transforms=[ref_forward_matrix, inverse, forward_transform, forward_matrix],
+                                      invert_transform_flags=[True, False, False, False],
+                                      reference_image=reference,
+                                      input_image=input_image,
+                                      output_image=output_image)
+                if input_image == mask_file:
+                    cmd.inputs.interpolation = "NearestNeighbor"
+                cmd.run()
+                output_images.append(output_image)
+
     return t1_out, mask_out, transforms_out, reference_out
 
 
@@ -104,14 +101,16 @@ def main():
     t1_fns = glob.glob(
         os.path.abspath("./RSNA_ASNR_MICCAI_BraTS2021_TrainingData_16July2021/BraTS2021_*/BraTS2021_*_t1.nii.gz"))
     print("n:", len(t1_fns))
+    t1c_fns = list()
+    t2_fns = list()
+    flair_fns = list()
     mask_fns = list()
-    reg_mask_fns = list()
-    reg_mask_args = list()
     for t1_fn in t1_fns:
+        t1c_fns.append(t1_fn.replace("t1.", "t1ce."))
+        t2_fns.append(t1_fn.replace("t1.", "t2."))
+        flair_fns.append(t1_fn.replace("t1.", "flair."))
         mask_fn = t1_fn.replace("t1.", "seg.")
         mask_fns.append(mask_fn)
-    #        reg_mask_arg = "-x Null,{}".format(reg_mask_fn)
-    #        reg_mask_args.append(reg_mask_arg)
 
     reg_masker = MapNode(Function(function=create_reg_mask,
                                   input_names=["seg_file"],
@@ -119,45 +118,35 @@ def main():
                          name="CreateRegMasks",
                          iterfield=["seg_file"])
 
-    input_node = Node(IdentityInterface(["target", "t1s", "masks"]), name="inputnode")
+    input_node = Node(IdentityInterface(["target", "t1s", "t1cs", "t2s", "flairs", "masks"]), name="inputnode")
     input_node.inputs.t1s = t1_fns
+    input_node.inputs.t1cs = t1c_fns
+    input_node.inputs.t2s = t2_fns
+    input_node.inputs.flairs = flair_fns
     input_node.inputs.masks = mask_fns
     input_node.inputs.target = os.path.abspath("./SRI-24-Brain.nii.gz")
 
     wf.connect(input_node, "masks", reg_masker, "seg_file")
 
-    reg_node = MapNode(_RegistrationSynQuick(transform_type="so"),
+    reg_node = MapNode(_RegistrationSynQuick(transform_type="s"),
                        name="Registration", iterfield=["moving_mask", "moving_image"])
     wf.connect(input_node, "target", reg_node, "fixed_image")
     wf.connect(input_node, "t1s", reg_node, "moving_image")
     wf.connect(reg_masker, "reg_mask", reg_node, "moving_mask")
 
-    mixer = Node(Function(function=mix_n_match,
-                          output_names=["t1_out", "mask_out", "transforms_out", "reference_out"]),
-                 name="MixNMatch")
-    wf.connect(reg_node, "forward_warp_field", mixer, "forward_transforms")
+    mixer = MapNode(Function(function=mix_n_match,
+                             output_names=["t1_out", "mask_out", "transforms_out", "reference_out"]),
+                    name="MixNMatch", iterfield=["t1_file", "mask_file", "forward_transform", "forward_matrix"])
+    wf.connect(reg_node, "forward_warp_field", mixer, "forward_transform")
     wf.connect(reg_node, "inverse_warp_field", mixer, "inverse_transforms")
-    wf.connect(input_node, "t1s", mixer, "t1_files")
-    wf.connect(input_node, "masks", mixer, "mask_files")
-
-    transform_t1s = MapNode(ApplyTransforms(), name="TransformT1s",
-                            iterfield=["reference_image", "input_image", "transforms"])
-    wf.connect(mixer, "t1_out", transform_t1s, "input_image")
-    wf.connect(mixer, "reference_out", transform_t1s, "reference_image")
-    wf.connect(mixer, "transforms_out", transform_t1s, "transforms")
-
-    transform_masks = MapNode(ApplyTransforms(interpolation="NearestNeighbor"), name="TransformMasks",
-                              iterfield=["reference_image", "input_image", "transforms"])
-    wf.connect(mixer, "mask_out", transform_masks, "input_image")
-    wf.connect(mixer, "reference_out", transform_masks, "reference_image")
-    wf.connect(mixer, "transforms_out", transform_masks, "transforms")
-
-    syncer = Node(Function(function=sync_outputs), name="SyncOutputs")
-    wf.connect(transform_t1s, "output_image", syncer, "warped_t1s")
-    wf.connect(transform_masks, "output_image", syncer, "warped_masks")
-    wf.connect(mixer, "mask_out", syncer, "mask_files")
-    wf.connect(mixer, "t1_out", syncer, "t1_files")
-    wf.connect(mixer, "reference_out", syncer, "reference_files")
+    wf.connect(reg_node, "out_matrix", mixer, "forward_matrix")
+    wf.connect(reg_node, "out_matrix", mixer, "forward_matrices")
+    wf.connect(input_node, "t1s", mixer, "t1_file")
+    wf.connect(input_node, "t1s", mixer, "reference_files")
+    wf.connect(input_node, "t1cs", mixer, "t1c_file")
+    wf.connect(input_node, "t2s", mixer, "t2_file")
+    wf.connect(input_node, "flairs", mixer, "flair_file")
+    wf.connect(input_node, "masks", mixer, "mask_file")
 
     wf.run(plugin="MultiProc", plugin_args={"n_procs": 40})
 
