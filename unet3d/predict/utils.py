00fa8d35b6b5021e924a03a0a37888cb07667f3a
David Ellis <david.ellis@unmc.edu>
2022-07-19 15:57:53 -0500

    DEBUG

--- unet3d/predict/utils.py
+++ unet3d/predict/utils.py
@@ -3,8 +3,8 @@ import numpy as np
 
 def pytorch_predict_batch_array(model, batch, n_gpus=1):
     import torch
-    print(batch.shape)
     batch_x = torch.as_tensor(batch)
+    print(batch_x.shape)
     pred_x = pytorch_predict_batch(batch_x, model, n_gpus)
     return np.moveaxis(pred_x.numpy(), 1, -1)
 
