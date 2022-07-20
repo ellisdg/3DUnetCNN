2ae1ced41ae3c61bd267258b6dc81f124ecd520c
david.ellis <david.ellis@unmc.edu>
2022-07-20 17:05:27 -0500

    ENH: modify dice loss to match traditional dice loss

--- unet3d/utils/pytorch/losses/losses.py
+++ unet3d/utils/pytorch/losses/losses.py
@@ -5,13 +5,16 @@ Put custom loss classes here.
 from torch import nn
 
 
-class MyDiceLoss(nn.Module):
-    # modified from wolny's GitHub project: https://github.com/wolny/pytorch-3dunet
-    def __init__(self, epsilon=1e-6, weight=None, dim=(0, 2, 3, 4)):
-        super(MyDiceLoss, self).__init__()
+class DiceLoss(nn.Module):
+    def __init__(self, epsilon=1e-6, weight=None, dim=(0, 2, 3, 4), v_net=False,
+                 smooth=1., generalized=True):
+        super(DiceLoss, self).__init__()
         self.epsilon = epsilon
         self.weight = weight
         self.dim = dim
+        self.v_net = v_net
+        self.smooth = smooth
+        self.generalized = generalized
 
     def forward(self, x, y):
         # input and target shapes must match
@@ -22,9 +25,17 @@ class MyDiceLoss(nn.Module):
         tp = (x * y).sum(dim=self.dim)
         if self.weight is not None:
             tp = self.weight * tp
-        fn = ((x == 0) * y).sum(dim=self.dim)
-        fp = (x * (y == 0)).sum(dim=self.dim)
 
-        # here we can use standard dice (input + target).sum(-1) or extension (see V-Net) (input^2 + target^2).sum(-1)
-        denominator = (2 * tp + fn + fp)
-        return 1 - (2 * (tp / denominator.clamp(min=self.epsilon))).mean()
+        if self.v_net:
+            denominator = (x * x).sum(dim=self.dim) + (y * y).sum(dim=self.dim)
+        else:
+            fn = ((x == 0) * y).sum(dim=self.dim)
+            fp = (x * (y == 0)).sum(dim=self.dim)
+            if self.generalized:
+                vol = y.sum(dim=self.dim)
+                tp = tp / vol
+                fn = fn / vol
+                fp = fp / vol
+            denominator = (2 * tp + fn + fp)
+
+        return 1 - ((2 * tp + self.smooth) / (denominator.clamp(min=self.epsilon) + self.smooth)).mean()
