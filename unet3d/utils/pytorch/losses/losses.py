8ecab7b767900cbfde889110c01ec6e6c9c5f115
david.ellis <david.ellis@unmc.edu>
2022-07-20 15:44:39 -0500

    ENH: modify dice loss to match traditional dice loss

--- unet3d/utils/pytorch/losses/losses.py
+++ unet3d/utils/pytorch/losses/losses.py
@@ -7,50 +7,24 @@ from torch import nn
 
 class MyDiceLoss(nn.Module):
     # modified from wolny's GitHub project: https://github.com/wolny/pytorch-3dunet
-    def __init__(self, epsilon=1e-6, weight=None):
+    def __init__(self, epsilon=1e-6, weight=None, dim=(0, 2, 3, 4)):
         super(MyDiceLoss, self).__init__()
         self.epsilon = epsilon
         self.weight = weight
+        self.dim = dim
 
     def forward(self, x, y):
-        """
-        Computes DiceCoefficient as defined in https://arxiv.org/abs/1606.04797 given  a multi channel input and target.
-        Assumes the input is a normalized probability, e.g. a result of Sigmoid or Softmax function.
-        Args:
-             x (torch.Tensor): NxCxSpatial input tensor
-             y (torch.Tensor): NxCxSpatial target tensor
-             epsilon (float): prevents division by zero
-             weight (torch.Tensor): Cx1 tensor of weight per channel/class
-        """
-
         # input and target shapes must match
         assert x.size() == y.size(), "'input' ({}) and 'target' ({}) must have the same shape".format(
             x.size(), y.size())
 
-        x = flatten(x)
-        y = flatten(y)
-        y = y.float()
-
         # compute per channel Dice Coefficient
-        intersect = (x * y).sum(-1)
+        tp = (x * y).sum(dim=self.dim)
         if self.weight is not None:
-            intersect = self.weight * intersect
+            tp = self.weight * tp
+        fn = ((x == 0) * y).sum(dim=self.dim)
+        fp = (x * (y == 0)).sum(dim=self.dim)
 
         # here we can use standard dice (input + target).sum(-1) or extension (see V-Net) (input^2 + target^2).sum(-1)
-        denominator = (x * x).sum(-1) + (y * y).sum(-1)
-        return 1 - (2 * (intersect / denominator.clamp(min=self.epsilon))).mean()
-
-
-def flatten(tensor):
-    """Flattens a given tensor such that the channel axis is first.
-    The shapes are transformed as follows:
-       (N, C, D, H, W) -> (C, N * D * H * W)
-    """
-    # number of channels
-    C = tensor.size(1)
-    # new axis order
-    axis_order = (1, 0) + tuple(range(2, tensor.dim()))
-    # Transpose: (N, C, D, H, W) -> (C, N, D, H, W)
-    transposed = tensor.permute(axis_order)
-    # Flatten: (C, N, D, H, W) -> (C, N * D * H * W)
-    return transposed.contiguous().view(C, -1)
+        denominator = (2 * tp + fn + fp)
+        return 1 - (2 * (tp / denominator.clamp(min=self.epsilon))).mean()
