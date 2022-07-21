909c7f01db3aa69f533a915fb88f55f94e1a66bc
david.ellis <david.ellis@unmc.edu>
2022-07-21 09:28:27 -0500

    FIX: dice loss

--- unet3d/utils/pytorch/losses/losses.py
+++ unet3d/utils/pytorch/losses/losses.py
@@ -6,13 +6,10 @@ from torch import nn
 
 
 class DiceLoss(nn.Module):
-    def __init__(self, epsilon=1e-6, weight=None, dim=(0, 2, 3, 4), v_net=False,
-                 smooth=1., generalized=True):
+    def __init__(self, weight=None, dim=(0, 2, 3, 4), smooth=1., generalized=True):
         super(DiceLoss, self).__init__()
-        self.epsilon = epsilon
         self.weight = weight
         self.dim = dim
-        self.v_net = v_net
         self.smooth = smooth
         self.generalized = generalized
 
@@ -26,17 +23,14 @@ class DiceLoss(nn.Module):
         if self.weight is not None:
             tp = self.weight * tp
 
-        if self.v_net:
-            denominator = (x * x).sum(dim=self.dim) + (y * y).sum(dim=self.dim)
-        else:
-            fn = ((x == 0) * y).sum(dim=self.dim)
-            fp = (x * (y == 0)).sum(dim=self.dim)
-            if self.generalized:
-                vol = y.sum(dim=self.dim)
-                tp = tp / vol
-                fn = fn / vol
-                fp = fp / vol
-            denominator = (2 * tp + fn + fp)
+        fn = ((1 - x) * y).sum(dim=self.dim)
+        fp = (x * (1 - y)).sum(dim=self.dim)
+        if self.generalized:
+            vol = y.sum(dim=self.dim) + self.smooth
+            tp = tp / vol
+            fn = fn / vol
+            fp = fp / vol
+        denominator = (2 * tp + fn + fp)
 
         return 1 - ((2 * tp + self.smooth) / (denominator + self.smooth)).mean()
 
