2beff6f7d654cc29a15acf032d25dfc1de20bcbb
david.ellis <david.ellis@unmc.edu>
2023-03-01 22:01:17 -0600

    ENH: don't change the affine if the shape matches the target shape

--- unet3d/utils/affine.py
+++ unet3d/utils/affine.py
@@ -38,12 +38,13 @@ def adjust_affine_spacing(affine, new_spacing, spacing=None):
 
 
 def resize_affine(affine, shape, target_shape, copy=True):
-    if copy:
-        affine = affine.detach().clone()
-    scale = torch.divide(torch.as_tensor(shape), torch.as_tensor(target_shape))
-    spacing = get_spacing_from_affine(affine)
-    target_spacing = torch.multiply(spacing, scale)
-    affine = adjust_affine_spacing(affine, target_spacing)
+    if shape != target_shape:
+        if copy:
+            affine = affine.detach().clone()
+        scale = torch.divide(torch.as_tensor(shape), torch.as_tensor(target_shape))
+        spacing = get_spacing_from_affine(affine)
+        target_spacing = torch.multiply(spacing, scale)
+        affine = adjust_affine_spacing(affine, target_spacing)
     return affine
 
 
