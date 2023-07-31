3fc342a8ac9e83185984f42ae85f1d3ec7bd8922
david.ellis <david.ellis@unmc.edu>
2023-07-31 18:45:52 -0500

    FIX

--- unet3d/scripts/script_utils.py
+++ unet3d/scripts/script_utils.py
@@ -245,11 +245,8 @@ def setup_cross_validation(config, work_dir, n_folds, random_seed=25):
 
 def load_filenames_from_config(config):
     for key in config:
-        if "_filenames" in config:
-            print(type(config[key]))
+        if "_filenames" in key:
             config[key] = load_filenames(config[key])
-            print(key)
-            print(type(config[key]))
 
 
 def load_filenames(filenames):
