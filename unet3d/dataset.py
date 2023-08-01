6dda260d0a7d3a96da473aad246860e506e5f278
david.ellis <david.ellis@unmc.edu>
2023-08-01 16:18:58 -0500

    Normalization

--- unet3d/dataset.py
+++ unet3d/dataset.py
@@ -1,10 +1,12 @@
+import monai
 from monai.data import PersistentDataset
-from monai.transforms import LoadImageD, ResizeWithPadOrCropD, Compose
+from monai.transforms import LoadImageD, ResizeWithPadOrCropD, Compose, NormalizeIntensityD
 from unet3d.transforms import LabelMapToOneHotD
 
 
 class SegmentationDatasetPersistent(PersistentDataset):
-    def __init__(self, filenames, cache_dir, labels=None, inference="auto", desired_shape=None):
+    def __init__(self, filenames, cache_dir, labels=None, inference="auto", desired_shape=None,
+                 normalization="zero_mean", normalization_kwargs=None):
         transforms = list()
         if inference == "auto":
             # Look at the first set and determine if labels are present
@@ -19,6 +21,18 @@ class SegmentationDatasetPersistent(PersistentDataset):
         if desired_shape:
             transforms.append(ResizeWithPadOrCropD(keys=keys, spatial_size=desired_shape, lazy=True))
 
+        if normalization_kwargs is None:
+            normalization_kwargs = dict()
+        if normalization is not None:
+            if normalization == "zero_mean":
+                normalization_class = NormalizeIntensityD
+                transforms.append(NormalizeIntensityD(keys=("image",), **normalization_kwargs))
+            else:
+                try:
+                    normalization_class = getattr(monai.transforms, normalization)
+                except ValueError:
+                    raise ValueError("{} normalization method not yet implemented".format(normalization))
+            transforms.append(normalization_class(keys=("image",), **normalization_kwargs))
         if not inference:
             if labels is None:
                 raise ValueError("Must set 'labels' for {}".format(self.__class__))
