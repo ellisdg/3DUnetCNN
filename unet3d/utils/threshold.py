ef4d7139b35bf432153847b326122ebe0cbe9c60
david.ellis <david.ellis@unmc.edu>
2023-08-01 18:29:45 -0500

    DEBUG

--- unet3d/utils/threshold.py
+++ unet3d/utils/threshold.py
@@ -5,6 +5,7 @@ def percentile_threshold(image, percentile):
     """
     image: (batch, channels, x, y, z)
     """
+    print(image.shape)
     # get the foreground based on the percentile for each channel of each batch input
     mask = image > torch.quantile(torch.flatten(image, start_dim=-3), percentile, dim=-1)[..., None, None, None]
     # if any of the channels has foreground at a given voxel, we want to keep that voxel in the mask
