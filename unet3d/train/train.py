3697bb2465de55ea06878aa621dde6e9ce8bfc56
david.ellis <david.ellis@unmc.edu>
2023-08-29 14:40:39 -0500

    ENH: allows for multiple training iterations per epoch

--- unet3d/train/train.py
+++ unet3d/train/train.py
@@ -11,7 +11,7 @@ from .training_utils import epoch_training, epoch_validation
 def run_training(model, optimizer, criterion, n_epochs, training_loader, validation_loader, training_log_filename,
                  model_filename, metric_to_monitor="val_loss", early_stopping_patience=None, save_best=False, n_gpus=1,
                  save_every_n_epochs=None, save_last_n_models=None, amp=False, scheduler=None, samples_per_epoch=None,
-                 inferer=None):
+                 inferer=None, training_iterations_per_epoch=1):
     training_log = list()
     if os.path.exists(training_log_filename):
         training_log.extend(pd.read_csv(training_log_filename).values)
@@ -50,8 +50,11 @@ def run_training(model, optimizer, criterion, n_epochs, training_loader, validat
             break
 
         # train the model
-        loss = epoch_training(training_loader, model, criterion, optimizer=optimizer, epoch=epoch, n_gpus=n_gpus,
-                              scaler=scaler, samples_per_epoch=samples_per_epoch)
+        losses = list()
+        for i in range(training_iterations_per_epoch):
+            losses.append(epoch_training(training_loader, model, criterion, optimizer=optimizer, epoch=epoch,
+                                         n_gpus=n_gpus, scaler=scaler, samples_per_epoch=samples_per_epoch))
+        loss = np.mean(losses)
 
         # Clear the cache from the GPUs
         if n_gpus:
