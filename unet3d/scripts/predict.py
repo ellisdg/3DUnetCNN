4af4857f15d487f6a1f915b88447a6d4e8da6f46
David G Ellis <david.ellis@unmc.edu>
2023-09-01 05:59:33 -0500

    some files for sppin

--- unet3d/scripts/predict.py
+++ unet3d/scripts/predict.py
@@ -66,7 +66,7 @@ def run_inference(config, output_directory, model_filename, group, activation, s
 
     dataloader = build_inference_loader(filenames=config[key],
                                         dataset_class=dataset_class,
-                                        dataset_kwargs=get_kwargs(config["dataset"]),
+                                        dataset_kwargs=get_kwargs(config["dataset"], ["name", "training", "validation"]),
                                         inference_kwargs=inference_dataset_kwargs,
                                         batch_size=batch_size,
                                         num_workers=in_config("n_workers", system_config, 1),
