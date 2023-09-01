0610682b54545a71a86fd1514e478c210624adc4
david.ellis <david.ellis@unmc.edu>
2023-09-01 06:19:51 -0500

    FIX: system_config

--- examples/sppin/process.py
+++ examples/sppin/process.py
@@ -248,7 +248,7 @@ def predict(*, inputs: Inputs) -> Outputs:
                                          output_directory=_output_dir,
                                          model_filename="/model/model.pth",
                                          group="test", activation="sigmoid",
-                                         system_config={"ngpus": 1, "nthreads": 8})
+                                         system_config={"n_gpus": 1, "n_workers": 8})
         prediction_images.append(nib.load(output_filenames[0]))
 
     prediction_filename = os.path.join(_tmp_dir, "predictions", "prediction.nii.gz")
